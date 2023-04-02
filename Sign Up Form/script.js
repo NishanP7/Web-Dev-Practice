94b3cfafd994b3b3bd342ebfbc346f8df25fcc32
Nishan Poudel <nishan.poudel7@gmail.com>
2023-04-02 12:51:09 +0545

    js-day-3

--- Sign Up Form/script.js
+++ Sign Up Form/script.js
@@ -9,22 +9,33 @@ submitButton.addEventListener("click",function(e){
    let passwordInput = signupForm.password;
 
    if(firstNameInput.value == ""){
-    errors.firstName = "First Name should not be Empty."
-    
+    errors.firstName = "First Name should not be empty."
+   }
+   if(lastNameInput.value == ""){
+    errors.lastName = "Last Name should not be empty."
+   }
+   if(emailInput.value ==""){
+    errors.email="Email Address should not be empty."
+   }
+   if(passwordInput.value ==""){
+    errors.password="Password should not be empty."
+   }
+   if(passwordInput.value.length <= 5){
+    errors.password="Password should be 5 characters minimum"
    }
 
-   if(lastNameInput.value == ""){
-    errors.lastName = "Last Name should not be Empty."
-    // let lastNameGroup = document.getElementById("lastNameGroup");
-    // let lastNameMessage = document.querySelector("#lastNameGroup .message");
-    // lastNameGroup.classList.add("error");
-    // lastNameMessage.textContent="Last Name should not be Empty."
-   }
    console.log(errors);
    for (let error in errors){
-
+    let lastNameGroup = document.getElementById(`${error}Group`);
+    let lastNameMessage = document.querySelector(`#${error}Group .message`);
+    lastNameGroup.classList.add("error");
+    lastNameMessage.textContent=errors[error];
+   }
+   if(Object.keys(errors).length == 0){
+    signupForm.submit();
    } 
-   
+});
+
 //    if(emailInput.value == ""){
 //     let emailGroup = document.getElementById("emailGroup");
 //     let emailMessage = document.querySelector("#emailGroup .message");
@@ -38,5 +49,4 @@ submitButton.addEventListener("click",function(e){
 //     passwordGroup.classList.add("error");
 //     passwordMessage.textContent="Password should not be Empty."
 //    }
-});
  
